25d4d037dc94857cfe505f05eecc5d3c39fb4669
Abdul-Kabeer <91217816+Abdul-Kabeer@users.noreply.github.com>
2021-10-01 07:41:59 +0200

    Add files via upload

--- task0.6.py
+++ task0.6.py
@@ -1,17 +1,8 @@
-nums = []
-def max_out(nums):
-    if ( nums[0] >= nums[1] ) and ( nums[0] >= nums[2] ):
-        largest = nums[0]
-    if ( nums[1] >= 0 ) and ( nums[1] >= nums [2] ):
-        largest = nums[1]
-    if ( nums[2] >= nums[0] ) and ( nums[2] >= nums[1] ):
-        largest = nums[2]
-    return largest
-nums = [12, 5, 8]
-print (max_out(nums))
-
-def max_out():list(12,5,8)
-list = (12,5,8)
-for x in list:
-        if x >= range(1, 20):
-            print(x)
+mainlist = (12,5,8)
+def max_out(out):
+    ilist = []
+    for x in out:
+        if x >= 0:
+            ilist.append(x)
+            return ilist
+print(max_out(mainlist))
