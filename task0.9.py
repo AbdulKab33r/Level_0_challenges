5e9a113baac96b34c8584068954c2a758bb7248f
Abdul-Kabeer <91217816+Abdul-Kabeer@users.noreply.github.com>
2021-10-01 06:27:46 +0200

    Add files via upload

--- task0.9.py
+++ task0.9.py
@@ -1,7 +1,17 @@
 vow_ex = ("Umuzi welcomes us")
 vowels =['a','e','i','o','u','A','E','I','O','U']
-list1=[]
+list=[]
 for x in vow_ex:
-    if (x in vowels and x not in list1):
-        list1.append(x)  
-print("Vowels present in given statement : ",list1)
+    if (x in vowels):
+        list.append(x)  
+print(list)
+
+def vow_ex(input):
+    vowels =['a','e','i','o','u','A','E','I','O','U']
+list=[]
+for x in vow_ex:
+    if (x in vowels):
+        list.append(x)  
+print(list)
+z = vow_ex
+print (z)
