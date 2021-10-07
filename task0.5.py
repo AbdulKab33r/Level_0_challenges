80655308fbfc5ff80a8bff18127e7dcca01cd08e
Abdul-Kabeer <91217816+Abdul-Kabeer@users.noreply.github.com>
2021-10-07 13:11:47 +0200

    Add files via upload

--- task0.5.py
+++ task0.5.py
@@ -1,5 +1,5 @@
 def triangle_area(side1, side2, side3): 
-    '''This will print the area of a trianlge using the side lengths'''
+    '''This will print the area of a trianlge using the side lengths method'''
     print ('The area of this triangle is:')
     return (side1 + side2 + side3) * 1/2
 
